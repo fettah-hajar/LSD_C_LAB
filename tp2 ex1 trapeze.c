afc8bf54fc7e1939f3a2a68c1c2510bcbd312578
hajar1-laaouina <76403949+hajar1-laaouina@users.noreply.github.com>
2021-01-25 02:05:17 +0100

    Update tp2 ex1 trapeze.c

--- tp2 ex1 trapeze.c
+++ tp2 ex1 trapeze.c
@@ -1,1 +1,34 @@
+#include<stdio.h>
 
+double f(double t)
+{
+	return t;
+}
+
+double Trapeze(double a, double b, int n)
+{
+	int i;
+	double h = (b - a) / n;
+	double s = (f(a) + f(b)) / 2;
+	for(i = 1; i < n; i++)
+	{
+	
+s+= f(a + i * h);
+	}
+	return h * s;
+}
+
+int main()
+{
+	double a, b, x;
+	int n;
+	printf("Values of a is : ");
+	scanf("%lf", &a);
+	printf("Values of b is : ");
+	scanf("%lf", &b);
+	printf("Values of n is : ");
+	scanf("%d", &n);
+	x = Trapeze(a, b, n);
+	printf("Result of Trapeze is : %lf \n", x);
+}
+		
