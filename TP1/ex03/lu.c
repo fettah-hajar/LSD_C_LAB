4cc97dc50ba74a23c3ac2a629204c7f9b520ff2a
hajar1-laaouina <76403949+hajar1-laaouina@users.noreply.github.com>
2021-01-25 01:15:30 +0100

    Update lu.c

--- TP1/ex03/lu.c
+++ TP1/ex03/lu.c
@@ -5,11 +5,40 @@
 float *lu(float A[N][N], float B[N], int n)
 {
 
-  /******Implement the solution here******/
-  /***************************************/
+  /* Elimination de Gauss sur A pour construire L et U */
+ for (k =0; k< n- 1; k++)
+ {
+   for (i = k+1; i< n; i++)
+   {
+     L[i][k] = A[i][k] / A[k][k];
+     for (j = k+1; j < n; j++)
+     {
+       A[i][j] = A[i][j] - L[i][k] * A[k][j];
+     }
+   }
+ }
 
+/* calcul de y avec LY = B(Methode de descente) : */
+  Y[0] = B[0];
+  for (i = 1; i < n; i++)
+  {
+    s = 0;
+    for (j = 0; j <= i-1; j++)
+    {
+      s += L[i][j] * Y[j];
+    }
+    Y[i] = B[i] - s;
+  }
 
-  /***************************************/
+/*calcul de x avec UX = Y (Methode de remontee) : */
+  X[n - 1] = Y[n - 1] / A[n - 1][n - 1];
+  for (i = n - 2; i>= 0; i--)
+  {
+    s = 0;
+    for (j = i+1; j <= n -1; j++)
+      s += A[i][j] * X[j];
+    X[i] = (Y[i] - s) / A[i][j];
+  }  
   return (x);
 }
 
